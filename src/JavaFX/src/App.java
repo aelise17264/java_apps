fcb06fa128d5314af9f156e7ec8b3be74d3a8e36
Anna <aelise17264@gmail.com>
2024-01-15 13:35:27 -0700

    jfx window working now, why? who knows

--- src/JavaFX/src/App.java
+++ src/JavaFX/src/App.java
@@ -1,18 +1,35 @@
+
+import java.awt.Font;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
+public class app extends Application {
+
+    public void start(Stage myStage){
+        StackPane root = new StackPane();
+        Text text = new Text("Hello from JavaFX");
+        // text.setFont(Font.font("Arial", 12));
+        text.setTranslateX(text.getTranslateX() + 100 );
+
+        root.getChildren().add(text);
+
+        Scene scene = new Scene(root, 320, 240);
+
+        myStage.setScene(scene);
+        myStage.setTitle("Title");
+        myStage.show();
+        
+    
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }

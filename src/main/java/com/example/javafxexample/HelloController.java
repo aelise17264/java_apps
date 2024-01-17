7617651e128e26005f2bac0e5a56dd08102435d1
Anna <aelise17264@gmail.com>
2024-01-17 12:19:47 -0700

    got user input from textfield

--- src/main/java/com/example/javafxexample/HelloController.java
+++ src/main/java/com/example/javafxexample/HelloController.java
@@ -2,13 +2,18 @@ package com.example.javafxexample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField textField;
+
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome " + textField.getText());
     }
 }

912c3c8bfbbf8888c2d20db91933389eb3d25967
Anna <aelise17264@gmail.com>
2024-01-18 11:14:32 -0700

    maven vs gradle deps set up

---
+++ GradleDeps/src/main/java/NumberHelperTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberHelperTest {
+
+    @Test
+    void addNumbers() {
+        NumberHelper numberHelper = new NumberHelper();
+        assertEquals(1, numberHelper.addNumbers(0, 1));
+        assertEquals(5, numberHelper.addNumbers(2, 3));
+        assertEquals(9, numberHelper.addNumbers(4, 5));
+    }
+}
